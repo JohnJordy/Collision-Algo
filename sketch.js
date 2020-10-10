ec7e519826c649b14745c2e2c64fa08534f9a694
JohnJordy <72341384+JohnJordy@users.noreply.github.com>
2020-10-10 16:56:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var movingrect 
+var fixedrect
+function setup() {
+ createCanvas(800,400);
+ movingrect = createSprite(400, 200, 50, 50);
+ fixedrect = createSprite(200, 400, 50, 50);
+}
+
+function draw() {
+  background(0);  
+  movingrect.x=World.mouseX
+  movingrect.y=World.mouseY
+  if (movingrect.x-fixedrect.x<fixedrect.width/2+movingrect.width/2&&fixedrect.x-movingrect.x<movingrect.width/2+fixedrect.width/2&&
+    movingrect.y-fixedrect.y<fixedrect.height/2+movingrect.height/2&&fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2){
+   movingrect.shapeColor="yellow" 
+   fixedrect.shapeColor="yellow" 
+  }
+  else {
+  movingrect.shapeColor =  "teal"
+  fixedrect.shapeColor =  "teal"
+  }
+drawSprites();
+}
